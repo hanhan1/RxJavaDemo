9e1a7e453ea287844a1d45a9ac333cbda7f629fb
hanhanhuang <549551740@qq.com>
2017-03-27 17:57:42 +0800

    初步使用RxJava

--- RxJavaDemos/app/src/main/java/com/hyh/rxjavademos/MainActivity.java
+++ RxJavaDemos/app/src/main/java/com/hyh/rxjavademos/MainActivity.java
@@ -2,6 +2,10 @@ package com.hyh.rxjavademos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String tag="hhh";
+        Observable observable = Observable.create(new Observable.OnSubscribe() {
+            @Override
+            public void call(Object o) {
+               Log.d(tag,"call"+o);
+            }
+        });
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String o) {
+                Log.d(tag,"hello world" );
+            }
+        };
+        observable.subscribe(subscriber);
     }
+
+
+
+
 }
